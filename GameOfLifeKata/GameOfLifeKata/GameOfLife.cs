ee88bb2360e9d64e30848511af00b74840da08e3
Iftekhar Ahmed <Iftekhar.Ahmed@asos.com>
2018-04-06 12:18:22 +0100

    First commit;

---
+++ GameOfLifeKata/GameOfLifeKata/GameOfLife.cs
@@ -0,0 +1,92 @@
+﻿namespace GameOfLifeKata
+{
+    using System;
+    using System.Text;
+
+    public class GameOfLife
+    {
+        private Cell[,] grid;
+        private int xCord;
+        private int yCord;
+
+        public Cell[,] Grid => this.grid;
+        
+        public void Seed(int x, int y)
+        {
+            this.xCord = x;
+            this.yCord = y;
+
+            var random = new Random(5);
+
+            this.grid = new Cell[x,y];
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    var nextNumber = random.Next();
+
+                    if (nextNumber % 2 == 0)
+                    {
+                        this.grid[i, j] = new Cell(CellState.Alive);
+                    }
+                    else
+                    {
+                        this.grid[i, j] = new Cell(CellState.Dead);
+                    }
+                }
+            }
+        }
+
+        public string PrintState()
+        {
+            var output = new StringBuilder();
+            for (int i = 0; i < this.grid.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < this.grid.GetUpperBound(1); j++)
+                {
+                    output.Append(this.grid[i, j] + ", ");
+                }
+
+                output.AppendLine();
+            }
+
+            return output.ToString();
+        }
+
+        public void Tick()
+        {
+            var gridBeforeTick = this.grid;
+            var gridAfterTick = this.MoveOnTick();
+            this.grid = gridAfterTick;
+        }
+
+        private Cell[,] MoveOnTick()
+        {
+            return new Cell[this.xCord, this.yCord];
+        }
+
+        public class Cell
+        {
+            private readonly CellState state;
+
+            public Cell(CellState state)
+            {
+                this.state = state;
+            }
+
+            public CellState State { get; set; }
+
+            public override string ToString()
+            {
+                return this.state == CellState.Dead ? "D" : "A";
+            }
+        }
+
+        public enum CellState
+        {
+            Dead,
+            Alive
+        }
+    }
+}
---
+++ GameOfLifeKata/GameOfLifeKata/GameOfLifeKataTests.cs
@@ -0,0 +1,54 @@
+﻿namespace GameOfLifeKata
+{
+    using System;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class GameOfLifeKataTests
+    {
+        private GameOfLife gameOfLife;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.gameOfLife = new GameOfLife();
+
+            this.gameOfLife.Seed(8, 8);
+        }
+
+        [Test]
+        public void SeedGrid()
+        {
+            Assert.That(this.gameOfLife.Grid, Is.Not.Null);
+            Assert.That(this.gameOfLife.Grid.GetLength(0), Is.EqualTo(8));
+            Assert.That(this.gameOfLife.Grid.GetLength(1), Is.EqualTo(8));
+        }
+
+        [Test]
+        public void CellsAreSeeded()
+        {
+            Assert.That(this.gameOfLife.Grid[0, 1], Is.Not.Null);
+        }
+
+        [Test]
+        public void PrintStateOfGrid()
+        {
+            var printOut = this.gameOfLife.PrintState();
+
+            Assert.That(printOut, Is.Not.Null);
+
+            Console.WriteLine(printOut);
+        }
+
+        [Test]
+        public void EvolveAfterFirstTick()
+        {
+            var initialState = this.gameOfLife.Grid;
+            this.gameOfLife.Tick();
+            var nextState = this.gameOfLife.Grid;
+
+            Assert.That(nextState, Is.Not.EqualTo(initialState));
+        }
+    }
+}
